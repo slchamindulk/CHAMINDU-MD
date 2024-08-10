d2b64eb5dcaf299cef8e3daa55abd324f5b77e90
slchamindulk <chamindulakshan193@gmail.com>
2024-08-10 23:34:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG:  process.env.ALIVE_IMG ||"https://telegra.ph/file/27fd62b63ee1069ca1baa.jpg",   
+ALIVE_MSG:  process.env.ALIVE_MSG ||"hello, i am CHAMINDU MD i am alive now!",
 };
+
