47a5c46ea8369c1be17d49c37f2300cb62ca1020
slchamindulk <chamindulakshan193@gmail.com>
2024-08-22 17:39:16 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -31,10 +31,11 @@ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downloadurl = down.dl_url
 
-//send audio message  
+//send audio + document message  
 
 await conn.sendMessage(from,{audio: {url:downloadurl},mimetype:"audio/mpeg"},{qouted:mek})
-  
+ await conn.sendMessage(from,{document: {url:downloadurl},mimetype:"audio/mpeg"fileName:data + title ".mp3",caption:"ᴍᴀᴅᴇ ʙʏ ᴄʜᴀᴍɪɴᴅᴜ"},{qouted:mek}) 
+ 
 
   
 }catch(e){
@@ -43,4 +44,50 @@ reply('${e}')
 }
 }} 
 
+
+//========video_dl=========\\
+
+cmd({
+    pattern: "video",
+    desc: "download videos ",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or name")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url 
+
+let desc = '
+⬇️ CHAMINDU-MD VIDEO DOWNLOADER ⬇️  
+  
+ title: ${data.title}
+  description: ${data.description}
+  time: ${data.timestamp} 
+  ago: ${data.ago} 
+  views: ${data.views}
+
+ᴍᴀᴅᴇ ʙʏ ᴄʜᴀᴍɪɴᴅᴜ 👑
+    '
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek}):
+
+//download video 
+let down = await fg.ytv(url)
+let downloadurl = down.dl_url
+
+//send video + document message  
+
+await conn.sendMessage(from,{video: {url:downloadurl},mimetype:"video/mp4"},{qouted:mek})
+ await conn.sendMessage(from,{document: {url:downloadurl},mimetype:"video/mp4"fileName:data + title ".mp4",caption:"ᴍᴀᴅᴇ ʙʏ ᴄʜᴀᴍɪɴᴅᴜ"},{qouted:mek}) 
+
+  
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+}} 
+
+    
   
